0b69bf0bc9c66c267996d71f101995fab18da1a8
rushi-desai <rushikeshdesai505@gmail.com>
2025-11-29 02:01:51 +0530

    backend: setup managers

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import { Socket } from "socket.io";
+const http = require('http');
+const express = require('express');
+
+
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(http);
+const io = new Server(server);
+
+
+io.on('connection', (socket:Socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
